47b489b8d6614bc933ece39c456c88cd2d24b27c
MargotHuet <margothuet4@gmail.com>
2023-12-12 13:11:49 +0000

    add one test

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -21,8 +21,8 @@ class PostFactory extends Factory
             "image_url" => $this->faker->imageUrl,
             "description" => $this->faker->sentence,
             "likes" => $this->faker->randomDigit,
-            "image_file" => $this->faker->image,
-            "user_id" => $this->faker->randomElement([1,2,3]),
+
+            "user_id" => $this->faker->numberBetween(1, User::count()),
         ];
     }
 }
--- tests/Feature/PostCardTest.php
+++ tests/Feature/PostCardTest.php
@@ -4,29 +4,41 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use App\Models\Post;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostCardTest extends TestCase
 {
 
-    use RefreshDatabase;
-
+    
+   /** @test */  
     /**
      * A basic feature test example.
      */
-    public function test_affichage_page_posts(): void
+    public function test_affichage_nom_User_posts(): void
     {
         $user = User::factory()->create();
 
-        $post = Post::factory()->create(['user_id' => $user->id,'description' => $user->id]);
+        $post_user_name = Post::factory()->create(['user_id' => $user->id]);
 
 
         $response = $this->actingAs($user)->get('/posts');
 
         $response->assertStatus(200);
         $response->assertSee(e($user->name)); // On teste si le nom de l'utilisateur pour un post, s'affiche sur la page.
-        $response->assertSee(e($user->description));
+    }
+
+    public function test_affichage_description_dun_post(): void 
+    {
+
+      //  $user = User::factory()->create();
+      //  $post = Post::factory()->create(); // Cela crée un post avec une description fictive
+    
+      //  $response = $this->actingAs($user)->get('/posts');
+    
+      //  $response->assertSee(e($post->description));
+
     }
 }
+
+    
